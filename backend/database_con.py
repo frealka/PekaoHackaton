38cc434c2387e1f6249cafc1c792ce4de01e32e3
Ubuntu <coder18@LinuxDSOsName.ay3keb23h34e1nqcdxazhtg1sf.ax.internal.cloudapp.net>
2019-10-26 09:20:54 +0000

    database con

---
+++ backend/database_con.py
@@ -0,0 +1,17 @@
+import pyodbc
+conn = pyodbc.connect('Driver={/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.2.so.0.1};'
+                      'Server=tcp:codersi06mssqlckwl3pv5lf5q4.database.windows.net,1433;'
+                      'Database=HackathonPOS;'
+                      'UID=codersi06;'
+                      'PWD=9D45PxuArXjF;')
+
+cursor = conn.cursor()
+
+headers_tpd = 'TPD.TxnData, TPD.TxnCzas, TPD.anon_kwota, TPD.DCC, TPD.TypePayment,'
+headers_merchant = 'MERCHANT.MCC, MERCHANT.LocCity, MERCHANT.LocZipCode '
+cursor.execute('SELECT TOP 10' + headers_tpd + headers_merchant + 'from TPD INNER JOIN MERCHANT ON TPD.anon_MID = anon_Eid')
+
+
+print(headers_tpd, headers_merchant)
+for row in cursor:
+    print(row)
